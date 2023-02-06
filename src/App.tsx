3a6f31e2e954336cccd243ec774c6f5b81b40d43
Alexsus <alexsuslin@inbox.ru>
2023-02-06 20:39:51 +0300

    add function removeTeam

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Todolist from "./Todolist";
 
@@ -7,14 +7,24 @@ export type PropsTypeTeam={
     title:string
     LH:boolean
 }
+export type FilterType='all'| 'winner' | 'loser'
 export function App(){
 
-    let team:PropsTypeTeam[]=[
+
+    let [team, setteam]=useState<PropsTypeTeam[]>([
         {id:1, title:'Barcelona', LH:true},
         {id:2, title:'Zenit', LH:false},
         {id:3, title:'Milan', LH:true},
         {id:4, title:'PAOK', LH:false},
-    ]
+    ])
+    const removeTeam=(id:number)=>{//удаляет команды
+       let a= team.filter(t=>t.id!==id)
+        setteam(a)
+    }
 
-    return<Todolist title={'FC'} team={team}/>
+    return<Todolist
+        title={'FC'}
+        team={team}
+        removeTeam={removeTeam}
+    />
 }
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,23 +1,24 @@
 import React, {FC} from 'react';
-import {PropsTypeTeam} from "./App";
+import {FilterType, PropsTypeTeam} from "./App";
 type PropsTyp={
     team:PropsTypeTeam[]
     title:string
+    removeTeam:(id:number)=>void
 }
 const Todolist:FC  <PropsTyp> = (props) => {
     return (
         <div>
             <h3>{props.title}</h3>
         <ul>
-            {props.team.map(t=><li>
+            {props.team.map(t=><li key={t.id}>
                 <span>{t.title}</span>
                 <input type='checkbox' checked={t.LH}/>
-
+                <button onClick={()=>props.removeTeam(t.id)}> x</button>
             </li>)}
         </ul>
-            <button> All</button>
-            <button> Winner</button>
-            <button> No Winner</button>
+            <button onClick={()=>{}}> All</button>
+            <button onClick={()=>{}}> Winner</button>
+            <button onClick={()=>{}}> Loser</button>
 
 </div>
     );
