d5fd55cdad6ef794f884db37ebe13b66163cc440
Alexsus <alexsuslin@inbox.ru>
2023-02-06 20:23:28 +0300

    render State

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,19 @@ import React from 'react';
 import './App.css';
 import Todolist from "./Todolist";
 
+export type PropsTypeTeam={
+    id:number
+    title:string
+    LH:boolean
+}
 export function App(){
 
-    return<Todolist/>
+    let team:PropsTypeTeam[]=[
+        {id:1, title:'Barcelona', LH:true},
+        {id:2, title:'Zenit', LH:false},
+        {id:3, title:'Milan', LH:true},
+        {id:4, title:'PAOK', LH:false},
+    ]
+
+    return<Todolist title={'FC'} team={team}/>
 }
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
-
-const Todolist = () => {
+import React, {FC} from 'react';
+import {PropsTypeTeam} from "./App";
+type PropsTyp={
+    team:PropsTypeTeam[]
+    title:string
+}
+const Todolist:FC  <PropsTyp> = (props) => {
     return (
         <div>
-            hello
-        </div>
+            <h3>{props.title}</h3>
+        <ul>
+            {props.team.map(t=><li>
+                <span>{t.title}</span>
+                <input type='checkbox' checked={t.LH}/>
+
+            </li>)}
+        </ul>
+            <button> All</button>
+            <button> Winner</button>
+            <button> No Winner</button>
+
+</div>
     );
 };
 
