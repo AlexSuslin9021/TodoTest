0ed323d6b6e18f741504475be58b1e4995e9d306
Alexsus <alexsuslin@inbox.ru>
2023-02-13 20:12:10 +0300

    add state

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Propstype} from "../data/dataState";
-type Propstypes={
-    page:Propstype
+
+type Propstypes = {
+    page: Propstype
 }
-export const Content = (props:Propstypes) => {
+export const Content = (props: Propstypes) => {
     return (
         <div>
-<h3>{props.page.item}</h3>
+            <h3>{props.page.item}</h3>
             <div>{props.page.price}</div>
 
         </div>
