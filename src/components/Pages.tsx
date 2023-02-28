1730f4d683705de24396d7c3763ab4350654d2f8
Alexsus <alexsuslin@inbox.ru>
2023-02-28 08:42:25 +0300

    add state in Pages

--- src/components/Pages.tsx
+++ src/components/Pages.tsx
@@ -2,15 +2,22 @@ import React from 'react';
 import {useParams} from "react-router-dom";
 import Content from "./Content";
 import {Propstype} from "../data/dataState";
+import {Error404} from "./pages/Error404";
 
 type Propstypesn={
     data:Propstype[]
 }
 const Pages = (props:Propstypesn) => {
     let params=useParams()
+
     return (
+
         <div>
-            <Content page={props.data[Number(params.id)]} />
+           {/* {props.data.length > Number(params.id) ?  <Content page={props.data[Number(params.id)]} />*/}
+           {/*     :*/}
+           {/*<Error404/> }*/}
+            <Content page={props.data[Number(params.id)]}/>
+
         </div>
     );
 };
