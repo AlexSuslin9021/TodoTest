7d81e7de3d4bdc22386cb520a7d9db3688e24bd3
Alexsus <alexsuslin@inbox.ru>
2023-03-01 22:34:30 +0300

    Create component Rating and Accordion

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
 
 
@@ -6,10 +6,30 @@ import './App.css';
 function App() {
     return (
         <div>
-
+This is App
+            <Rating/>
+            <Accordion/>
         </div>
     );
 }
 
 
+const Rating=()=>{
+return <div>
+    <div>Rating</div>
+    <div>Rating</div>
+    <div>Rating</div>
+    <div>Rating</div>
+</div>
+}
+const Accordion=()=>{
+    return <div>
+        <h3> Menu</h3>
+     <ul>
+         <li>1</li>
+         <li>2</li>
+         <li>3</li>
+     </ul>
+    </div>
+}
 export default App;
