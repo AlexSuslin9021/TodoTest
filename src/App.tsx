13dcbb417f4fc5f64925909881502c4180d5271c
Alexsus <alexsuslin@inbox.ru>
2023-03-02 22:11:03 +0300

    Add component OnOff.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import {Accordion} from "./COMPONENT/Accordion/Accordion";
 import {Rating} from "./COMPONENT/Rating/Rating";
+import {OnOff} from "./COMPONENT/onOff/OnOff";
 
 
 function App() {
@@ -12,8 +13,10 @@ function App() {
             <Rating value={3}/>
             <Rating value={4}/>
             <Rating value={5}/>
+
             <Accordion titleValue={'title1'} collapsed={true}/>
             <Accordion titleValue={'title2'} collapsed={false}/>
+            <OnOff />
         </div>
     );
 }
---
+++ src/COMPONENT/onOff/OnOff.tsx
@@ -0,0 +1,53 @@
+import {useState} from "react";
+//
+// type PropsOnOff={
+//     on:boolean
+//     // callback:(check:boolean)=>void
+//
+// }
+export function OnOff(){
+
+
+   const [on, setOn]=useState<boolean>(true)
+
+
+    const OnStyle={
+        width:'30px',
+        height: '20px',
+        borderRadius:'5px',
+        border:'1px solid black',
+        // marginLeft:'5px',
+        padding:'2px',
+        display:'inline-block',
+        background: on ? 'green' :'white'
+    }
+    const OfStyle={
+        width:'30px',
+        height: '20px',
+        borderRadius:'5px',
+        border:'1px solid black',
+        padding:'2px',
+        // marginLeft:'5px',
+        display:'inline-block',
+        background:on ? 'white' :'red'
+    }
+    const OStyle={
+        width:'15px',
+        height: '10px',
+        borderRadius:'5px',
+        border:'1px solid black',
+        marginLeft:'5px',
+        display:'inline-block',
+        background: on ? 'green' :'red'
+
+    }
+const onClickHandler=()=>{
+       setOn(!on)
+}
+
+    return<div>
+        <div style={OnStyle} onClick={onClickHandler} >on</div>
+        <div style={OfStyle} onClick={onClickHandler} >off</div>
+        <div style={OStyle}  onClick={onClickHandler} ></div>
+    </div>
+}
