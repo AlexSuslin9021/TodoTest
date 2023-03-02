763dc1a9b7858b3bc29b2b12e530858b5c2ca372
Alexsus <alexsuslin@inbox.ru>
2023-03-02 21:41:28 +0300

    Add props for Star and Rating

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,11 @@ import {Rating} from "./COMPONENT/Rating/Rating";
 function App() {
     return (
         <div>
-            <Rating/>
+            <Rating value={1}/>
+            <Rating value={2}/>
+            <Rating value={3}/>
+            <Rating value={4}/>
+            <Rating value={5}/>
             <Accordion titleValue={'title1'} collapsed={true}/>
             <Accordion titleValue={'title2'} collapsed={false}/>
         </div>
--- src/COMPONENT/Rating/Rating.tsx
+++ src/COMPONENT/Rating/Rating.tsx
@@ -1,19 +1,23 @@
-import React from "react";
+import React, {FC} from "react";
 
+type RatingProps = {
+    value: 0 | 1 | 2 | 3 | 4 | 5
+}
 
-
-export const Rating = () => {
+export const Rating: FC<RatingProps> = (props) => {
     return <div>
-        <Star selected={true}/>
-        <Star selected={true}/>
-        <Star selected={false}/>
-        <Star selected={false}/>
-        <Star selected={false}/>
-
+        <Star selected={props.value > 0}/>
+        <Star selected={props.value > 1}/>
+        <Star selected={props.value > 2}/>
+        <Star selected={props.value > 3}/>
+        <Star selected={props.value > 4}/>
 
     </div>
 }
 
-const Star = (props:any) => {
-    return  props.selected ? <span> <b> star </b></span> : <span> star </span>
+type StarProps = {
+  selected:boolean
+}
+const Star = (props: StarProps) => {
+    return props.selected ? <span> <b> star </b></span> : <span> star </span>
 }
